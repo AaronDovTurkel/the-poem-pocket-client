a1be86accebe965b0ee3d903abfc7877d80cb676
Aaron Dov Turkel <AaronDovTurkel@gmail.com>
2019-05-31 11:54:54 -0400

    set up config to work with api

---
+++ src/components/site-info.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
+
+import './site-info.css';
+
+export function SiteInfo(props) {
+    // If we are logged in redirect straight to the user's dashboard
+    if (props.loggedIn) {
+        return <Redirect to="/dashboard" />;
+    }
+
+    return (
+        <div className="site-info-container">
+            <div className="info-content">
+                <p>Hi! Welcome to "The Poetry Pocket".</p>
+                <p>This is a place for different communities to come togther and share ideas by creating a cohesive poem.</p>
+                <p>Every day a new poem title with an opening stanza will be posted.</p>
+                <p>On a first-come-first-serve basis, anyone who wishes will have a chance to contribute a stanza to the public poem.</p>
+                <p>The poem will close at a perscribed line number and be published to the public viewing board!</p>
+            </div>
+            <div className="register-link"><Link to="/register">Register Now!</Link></div>
+            <div className="log-in-link">Already have an account? <Link to="/landing">Log In</Link></div>
+        </div>
+    );
+}
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(SiteInfo);
